43e32aa21a4889d8f87ba7704ec496879308c7ca
Anand Baburajan <anandbaburajan@gmail.com>
2020-02-28 01:25:07 +0530

    Insert function and readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # ChunkedSet
+Implement a ChunkedSet - which is a set of size N that can store upto N unique numbers. The ChunkedSet stores its elements in Chunks of a fixed sized k.
+
+The various operations such as insert, remove, get_size etc.. are always performed on a chunk object. The chunks themselves may be placed on different computers in a network hence for each of the operations, one Chunk must corrdinate with other chunks to ensure that the overall ChunkedSet is valid.
+
+
+Installation
+------------
+
+```bash
+pip install -r requirements.txt
+```
+
+How to run?
+------------
+
+```bash
+python app.py
+```
+
+Use Postman or your any tool of your for API testing
+
+Endpoints
+------------
+
+```bash
+http://localhost:5000/api/insert/     #Post request JSON body format: {"num":1}
+http://localhost:5000/api/get_size/
+http://localhost:5000/api/remove/     #Post request JSON body format: {"num":1}
+http://localhost:5000/api/clear/
+```
---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify, abort
+
+app = Flask(__name__)
+
+class Chunk():   #Each chunk on a different computer
+    total_elements = 0   #Total number of unique numbers in ChunkedSet
+    def __init__(self, name, size):
+        self.name = name   #Name of chunk
+        self.size = size   #Max capacity of chunk (k)
+        self.data = set()   #Numbers stored in a set, implemented as a hashtable
+
+@app.route("/api/insert/", methods=['POST'])   #Endpoint for inserting a number
+def insert():   #Post request JSON body format: {"num":1}
+    if not request.json or not 'num' in request.json:
+        abort(400)
+    num = request.json['num']   #Retrieve number from request
+    if Chunk.total_elements<N:   #If there's space left to insert
+        for x in chunks:   #For each chunk
+             if num in x.data:   #Check if num is already there in the chunk
+                return jsonify({'response': 'Failed!'})
+        cur_chunk = min(chunks, key=lambda x: len(x.data))   #Choose smallest chunk
+        if len(cur_chunk.data) == k:   #Check if the chunk is full
+            return jsonify({'response': 'Failed!'})
+        cur_chunk.data.add(num)   #Add num to the chunk
+        Chunk.total_elements += 1   #Update total elements
+        return jsonify({'response': 'Successfully Inserted!'})
+    else:
+        return jsonify({'response': 'Failed!'})
+
+if __name__ == '__main__':
+    N = 9   #ChunkedSet of size
+    k = 3   #Maximum capacity of a chunk
+    A = Chunk("A",k)   #New chunk
+    B = Chunk("B",k)
+    C = Chunk("C",k)
+    chunks = [A,B,C]   #Iterable for chunks
+    app.run()
